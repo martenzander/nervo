c1cb45e67766546c7bd90442768189f2e84f2bf8
Marten <martenzander@gmail.com>
2019-04-01 14:22:05 +0200

    postcss-plugin squad update

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -5,59 +5,59 @@ module.exports = ctx => ({
 	map: ctx.options.map,
 	plugins: {
 		"postcss-import": {},
-		// "postcss-at-rules-variables": {},
-		// "postcss-nested-ancestors": {},
+		"postcss-at-rules-variables": {},
+		"postcss-nested-ancestors": {},
 		// "postcss-custom-properties": {},
 		// "postcss-custom-selectors": {},
-		// "postcss-selector-not": {},
-		// "postcss-for": {},
-		// "postcss-each": {},
+		"postcss-selector-not": {},
+		"postcss-for": {},
+		"postcss-each": {},
 		"postcss-sassy-mixins": {},
-		// "postcss-conditionals": {},
-		// "postcss-advanced-variables": {},
+		"postcss-conditionals": {},
+		"postcss-advanced-variables": {},
 		// "postcss-quantity-queries": {},
-		// "postcss-hexrgba": {},
+		"postcss-hexrgba": {},
 		// "postcss-object-fit-images": {},
-		// "postcss-pxtorem": {
-		// 	rootValue: 16,
-		// 	selectorBlackList: ["body"]
-		// },
+		"postcss-pxtorem": {
+			rootValue: 16,
+			selectorBlackList: ["body"],
+		},
 		// "postcss-assets": {
 		// 	loadPaths: ["assets/img/"]
 		// },
-		// "postcss-flexbugs-fixes": {},
-		// "postcss-size": {},
-		// "postcss-inline-svg": {
-		// 	path: "src/svg"
-		// },
-		// "postcss-will-change": {},
-		// "postcss-nested": {},
+		"postcss-flexbugs-fixes": {},
+		"postcss-size": {},
+		"postcss-inline-svg": {
+			path: "app/src/svg",
+		},
+		"postcss-will-change": {},
+		"postcss-nested": {},
 		// "postcss-random": {},
-		// "postcss-calc": {
-		// 	precision: 10,
-		// 	warnWhenCannotResolve: false,
-		// 	mediaQueries: true
-		// },
-		// "postcss-initial": {},
+		"postcss-calc": {
+			precision: 10,
+			warnWhenCannotResolve: false,
+			mediaQueries: true,
+		},
+		"postcss-initial": {},
 		// "postcss-input-style": {},
-		// "postcss-easings": {},
+		"postcss-easings": {},
 		"postcss-mesh": {},
 		autoprefixer: {
 			browsers,
 		},
 		// "postcss-svgo": svgoSettings,
-		// cssnano:
-		// 	ctx.env === "production"
-		// 		? {
-		// 				filterPlugins: false,
-		// 				safe: true,
-		// 				mergeRules: false,
-		// 				browsers,
-		// 				svgo: false,
-		// 				discardComments: {
-		// 					removeAll: true
-		// 				}
-		// 		  }
-		// 		: false
+		cssnano:
+			ctx.env === "production"
+				? {
+						filterPlugins: false,
+						safe: true,
+						mergeRules: false,
+						browsers,
+						svgo: false,
+						discardComments: {
+							removeAll: true,
+						},
+				  }
+				: false,
 	},
 });
