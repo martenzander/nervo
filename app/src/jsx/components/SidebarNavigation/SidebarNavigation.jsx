ffe0f7d0f80d6d56660c601aa51c911deb1618e0
Marten <martenzander@gmail.com>
2019-04-04 00:43:23 +0200

    scrollspy

--- app/src/jsx/components/SidebarNavigation/SidebarNavigation.jsx
+++ app/src/jsx/components/SidebarNavigation/SidebarNavigation.jsx
@@ -3,11 +3,50 @@ import React, { Component } from "react";
 const Link = React.lazy(() => import("./../Link/Link"));
 
 class SidebarNavigation extends Component {
+	constructor(props) {
+		super(props);
+		this.sections = props.parent.props.sections;
+		this.state = {
+			scrollPos: window.scrollY,
+		};
+	}
+
+	onScroll = e => {
+		this.setState({ scrollPos: window.scrollY });
+	};
+
+	onResize = e => {
+		this.setState({ scrollPos: window.scrollY });
+	};
+
+	componentDidMount = e => {
+		window.addEventListener("scroll", this.onScroll);
+		window.addEventListener("resize", this.onResize);
+	};
+
+	componentWillUnmount = e => {
+		window.removeEventListener("scroll", this.onScroll);
+		window.removeEventListener("resize", this.onResize);
+	};
+
+	updateSidebar = e => {};
+
 	render() {
 		const items = this.sections.map((s, i) => {
+			const sectionNode = document.querySelector(`#${s.key}`);
+			const sectionTop = sectionNode.getBoundingClientRect().top;
+			const sectionHeight = sectionNode.getBoundingClientRect().height;
+
+			if (sectionTop <= 60 && sectionTop + sectionHeight > 60) {
+				return (
+					<li key={s.key}>
+						<Link component={"sidebar"} isActive={true} value={s.key} target={`#${s.key}`} />
+					</li>
+				);
+			}
 			return (
 				<li key={s.key}>
-					<Link value={s.key} target={`#${s.key}`} />
+					<Link component={"sidebar"} value={s.key} target={`#${s.key}`} />
 				</li>
 			);
 		});
