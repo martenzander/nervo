9fe1bed08bfc1f29222c5e2aa8accd432bdb3e98
Marten <martenzander@gmail.com>
2019-04-03 22:05:40 +0200

    sticky sidebar

--- app/src/jsx/components/Sidebar/Sidebar.jsx
+++ app/src/jsx/components/Sidebar/Sidebar.jsx
@@ -5,13 +5,88 @@ const SidebarNavigation = React.lazy(() => import("./../SidebarNavigation/Sideba
 class Sidebar extends Component {
 	constructor(props) {
 		super(props);
+		this.stickyPadding = 60;
+		this.isFixed = false;
+		this.stickyLimit = null;
+		this.className = styles.sidebar;
 	}
 
-	render() {
+	refCallback = e => {
+		this.node = e;
+	};
+
+	onScroll = e => {
+		this.setState({
+			scroll: window.scrollY,
+		});
+	};
+
+	onResize = e => {
+		this.setState({
+			scroll: window.scrollY,
+		});
+	};
+
+	componentDidMount = e => {
+		window.addEventListener("scroll", this.onScroll);
+		window.addEventListener("resize", this.onResize);
+		this.main = document.querySelector(".js-main-content");
+	};
+
+	componentWillUnmount = e => {
+		window.removeEventListener("scroll", this.onScroll);
+		window.removeEventListener("resize", this.onResize);
+	};
+
+	getWidth = e => {
+		const parentPadding = window.getComputedStyle(this.node.parentElement)["padding-left"];
+
 		return (
-			<div className={styles.sidebar}>
+			parseFloat(this.node.parentElement.getBoundingClientRect().width) - parseFloat(parentPadding.substring(0, parentPadding.length - 2)) * 2
+		);
+	};
+
+	updateSideBar = e => {
+		if (!this.main) return;
+		this.mainTop = this.main.getBoundingClientRect().top;
+		this.mainHeight = this.main.getBoundingClientRect().height;
+		this.mainBottom = this.mainHeight - Math.abs(this.mainTop);
+		this.top = this.node.getBoundingClientRect().top;
+		this.height = this.node.getBoundingClientRect().height;
+		this.width = this.getWidth();
+
+		if (this.isFixed) {
+			this.stickyLimit = this.top + this.height;
+		}
+
+		if (this.mainTop <= 60 && this.mainBottom >= this.stickyLimit) {
+			this.sidebarInlineStyles = {
+				width: `${this.width}px`,
+				top: `${this.stickyPadding}px`,
+			};
+			if (this.isFixed) return;
+			this.className = `${styles.sidebar} ${styles.fixed}`;
+			this.isFixed = true;
+		} else {
+			if (!this.isFixed) return;
+			if (this.mainTop >= 0) {
+				this.node.parentElement.style.verticalAlign = "top";
+			} else {
+				this.node.parentElement.style.verticalAlign = "bottom";
+			}
+			this.sidebarInlineStyles = {};
+			this.className = `${styles.sidebar}`;
+			this.isFixed = false;
+		}
+	};
+
+	render() {
+		this.updateSideBar();
+
+		return (
+			<div className={this.className} ref={this.refCallback} style={this.sidebarInlineStyles}>
 				<Suspense fallback={<div>Loading SidebarNavigation Component</div>}>
-					<SidebarNavigation sections={this.props.sections} />
+					<SidebarNavigation parent={this} />
 				</Suspense>
 			</div>
 		);
