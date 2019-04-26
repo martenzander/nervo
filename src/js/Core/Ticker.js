b042e56ed512b3064de30a8012ced49032093e1f
Marten <martenzander@gmail.com>
2019-04-26 02:36:19 +0200

    comments on Nervo.Ticker()

--- src/js/Core/Ticker.js
+++ src/js/Core/Ticker.js
@@ -4,6 +4,10 @@ import Base from "./Base";
 import * as Eases from "eases";
 
 export default class Ticker extends Base {
+	/*
+		Nervo.Ticker: This is not intended to be called directly.
+		It is called internally as a base class to manage timing and easing by other Nervo classes.
+	*/
 	constructor(options) {
 		super(options);
 		this.autoStart = options.autoStart !== undefined ? options.autoStart : Nervo.AutoStart;
@@ -14,40 +18,60 @@ export default class Ticker extends Base {
 		this.isActive = false;
 		this.loop = options.loop !== undefined ? options.loop : Nervo.Loop;
 		this.timeScale = options.timeScale !== undefined ? options.timeScale : Nervo.TimeScale;
-
-		// event binding
-		this.tick = this.tick.bind(this);
 	}
 
+	/*
+		this.reset(): Resets this instance and all of its children to t = 0.
+		It's best practice to not call this method directly.
+		Use this.start() or this.stop() instead.
+	*/
+
 	reset() {
-		// reset currentTime
 		this.currentTime = 0;
 
-		// trigger reset of children
+		/* Reset all children. */
 		this.children.forEach(child => {
 			child.reset();
 		});
 
-		/* set this.isActive so this.update(t) will have effect */
+		/* Set this.isActive so this.update(t) will have effect. */
 		this.isActive = true;
 
 		this.update(this.currentTime);
 	}
+
+	/*
+		this.start(): Resets this instance, all of its children and than starts ticking.
+	*/
 
 	start() {
 		this.reset();
 		this.play();
 	}
 
+	/*
+		this.stop(): Resets this instance, all of its children and than stops at t = 0.
+	*/
+
 	stop() {
 		this.reset();
 		this.pause();
 	}
 
+	/*
+		this.pause(): Sets this.isActive to false and interrupts the tick loop.
+		Also stops the clock at this.currentTime.
+	*/
+
 	pause() {
 		this.isActive = false;
 		this.clock.stop();
 	}
+
+	/*
+		this.play(): Sets this.isActive to true and triggers the tick loop after
+		restarting the clock.
+	*/
 
 	play() {
 		this.isActive = true;
@@ -55,17 +79,28 @@ export default class Ticker extends Base {
 		this.tick();
 	}
 
+	/*
+		this.execute(): Actual performance code of extending classes belongs here.
+	*/
+
 	execute() {}
+
+	/*
+		this.update(): Updates the progress based on the currentTime.
+		Than applies timeScale and triggers this.execute() with final updateTime.
+		Lastly trigger events based on progress and loop options.
+	*/
 
 	update(t) {
 		if (!this.isActive) return;
 
 		let updateTime = 0;
-		// update progress
+
+		/* Update progress. */
 		this.progress = t / (this.duration * this.timeScale);
 		this.easedProgress = this.easing(this.progress);
 
-		// validate progress
+		/* Set actual updateTime. */
 		if (this.progress >= 1.0) {
 			this.progress = 1.0;
 			this.easedProgress = 1.0;
@@ -75,7 +110,8 @@ export default class Ticker extends Base {
 		}
 
 		this.execute(updateTime);
-		// onProgress
+
+		/* Trigger events based on progress and loop options. */
 		this.onProgress();
 
 		if (this.progress >= 1.0) {
@@ -88,17 +124,22 @@ export default class Ticker extends Base {
 		}
 	}
 
-	tick() {
+	/*
+		this.tick(): The requestAnimationFrame loop to trigger this.update() with currentTime.
+		Only gets triggered by this.play() and should not be called directly.
+	*/
+
+	tick = e => {
 		if (!this.isActive) return;
 
-		// update time
+		/* Update time. */
 		this.deltaTime = this.clock.getDelta();
 		this.lastTime = this.currentTime;
 		this.currentTime += this.deltaTime;
 
-		// update call
+		/* Trigger update to calculate actual progress. */
 		this.update(this.currentTime);
 
 		requestAnimationFrame(this.tick);
-	}
+	};
 }
