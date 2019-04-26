120778f448a84e88af38f82d19f3b9249dab74f3
Marten <martenzander@gmail.com>
2019-04-26 02:38:32 +0200

    formatting

--- app/src/data/about.js
+++ app/src/data/about.js
@@ -1,11 +1,12 @@
 const about = {
-  name: "About",
-  contents: [
-    {
-      component: "copy",
-      value: "There are a lot of different grid systems already out there and most of them are pretty good. But ☝️ none of them is offering the whole bandwidth of possible options. E.g. I wanted to switch between a flex, inline-block or float based grid as well as I wanted to be able to overwrite certain parameters like gutter or column-count breakpoint wise. That is how I came up with the idea to create my very own grid compiler and Mesh was born 🎉🎉🎉."
-    }
-  ]
-}
+	name: "About Nervo",
+	contents: [
+		{
+			component: "copy",
+			value:
+				"There are a lot of different grid systems already out there and most of them are pretty good. But ☝️ none of them is offering the whole bandwidth of possible options. E.g. I wanted to switch between a flex, inline-block or float based grid as well as I wanted to be able to overwrite certain parameters like gutter or column-count breakpoint wise. That is how I came up with the idea to create my very own grid compiler and Mesh was born 🎉🎉🎉.",
+		},
+	],
+};
 
-export default about
+export default about;
