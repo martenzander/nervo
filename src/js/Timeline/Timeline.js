826b7aeac8f8e164e789a86a029b730ea83f8181
Marten <martenzander@gmail.com>
2019-05-01 01:54:57 +0200

    formatting

--- src/js/Timeline/Timeline.js
+++ src/js/Timeline/Timeline.js
@@ -3,18 +3,18 @@ import Track from "./../Track/Track";
 import Ticker from "./../Core/Ticker";
 
 export default class Timeline extends Ticker {
+	@readonly
+	isTimeline = true;
+
+	@readonly
+	type = "Timeline";
+
 	constructor(objects = [], options = {}) {
 		super(options);
 
 		if (objects.length !== 0) this.add(objects, options);
 		if (this.autoStart) this.start();
 	}
-
-	@readonly
-	isTimeline = true;
-
-	@readonly
-	type = "Timeline";
 
 	@readonly
 	_execute = t => {
